3baf2001445b7ad02e07baf2ce8e5f367c486cdd
tithin <tithin@gmail.com>
2025-09-28 19:04:37 +1000

    D: Updated Main with Groups

--- main.py
+++ main.py
@@ -7,17 +7,21 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
--- player.py
+++ player.py
@@ -3,7 +3,7 @@ from constants import *
 from circleshape import CircleShape
 
 
-class Player(CircleShape):
+class Player(CircleShape):  
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
